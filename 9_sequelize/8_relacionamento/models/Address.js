296c6d8d340fa631a5c6d28bbc6e28b8bd0763fd
Filipe Muliterno <filipemuliternop@hotmail.com>
2023-09-13 22:13:38 -0300

    13 09

---
+++ 9_sequelize/8_relacionamento/models/Address.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+
+const db = require("../db/conn");
+
+const User = require("./User"); // requisitando a tabela user
+
+// cria a tabela adress
+const Address = db.define("Address", {
+  street: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  number: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  city: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+});
+
+// criacao da relacao
+Address.belongsTo(User); // um endereco pertence a um usuario
+
+module.exports = Address;
